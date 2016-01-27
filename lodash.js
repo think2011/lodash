5c2e32bb89d334fa22e1c2e94d978112965a5b62
think2011 <452125301@qq.com>
2016-01-27 22:58:46 +0800

    publish

--- lodash.js
+++ lodash.js
@@ -2731,6 +2731,7 @@
      * The base implementation of `_.invoke` without support for individual
      * method arguments.
      *
+     *
      * @private
      * @param {Object} object The object to query.
      * @param {Array|string} path The path of the method to invoke.
